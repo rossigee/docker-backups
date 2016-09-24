2ecf4a8c4b00fb79969d100e1facac1f0b71f0cf
Ross Golder <ross@golder.org>
2016-09-24 23:30:19 +0700

    Fix for warning re perms of client creds.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-echo "Configuring MySQL client settings..."
-cat >$HOME/.my.cnf <<EOF
+if [ -n "$MYSQL_NAME" ]; then
+    echo "Configuring MySQL client settings..."
+    cat >$HOME/.my.cnf <<EOF
 [client]
 host=$MYSQL_HOST
 port=$MYSQL_PORT
 user=$MYSQL_USER
 password=$MYSQL_PASS
 EOF
+    chmod 400 $HOME/.my.cnf
+fi
 
-echo "Configuring PostGresQL client settings..."
-cat >$HOME/.pgpass <<EOF
+if [ -n "$PGSQL_NAME" ]; then
+    echo "Configuring PostGresQL client settings..."
+    cat >$HOME/.pgpass <<EOF
 $PGSQL_HOST:4567:$PGSQL_NAME:$PGSQL_USER:$PGSQL_PASS
 EOF
+    chmod 400 $HOME/.pgpass
+fi
 
 # Warning fix...
 [ ! -d ~/.gnupg ] && mkdir ~/.gnupg && touch ~/.gnupg/gpg.conf
